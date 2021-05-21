6721399af4e5459c7835492ea2c5140c51e8a742
Sergej Sakac <sakacszergej@gmail.com>
2021-05-21 10:10:26 +0200

    hover animation for card
    hover animation for card item

--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,16 @@
 export default () => {
+    const addToCart = () => {
+        console.log('added to cart');
+    }
     return (
         <div className="card">
             <img src="./images/orange.png" />
             <div className="description">
                 <h3 className="card__title">Orange</h3>
                 <p className="price">4.99$</p>
+            </div>
+            <div className="card__modal">
+                <a onClick={addToCart}>Buy</a>
             </div>
         </div>
     )

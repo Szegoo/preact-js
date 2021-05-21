02ec88b8e6fa0e3572b7a4fde6fd4234b3b0653d
Sergej Sakac <sakacszergej@gmail.com>
2021-05-21 09:09:39 +0200

    Added an item element
    Added an item element. You can use it for webshop items.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LoginForm from './components/login-form';
 import AlertBox from './components/alert-box';
+import Card from './components/card';
 function App() {
   return (
     <div className="App">
       <LoginForm />
       <AlertBox />
+      <Card />
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+export default () => {
+    return (
+        <div className="card">
+            <img src="./images/orange.png" />
+            <div className="description">
+                <h3 className="card__title">Orange</h3>
+                <p className="price">4.99$</p>
+            </div>
+        </div>
+    )
+}

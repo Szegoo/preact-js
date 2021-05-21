ee0629974d946fc0cc542260320fa471c82ac437
Sergej Sakac <sakacszergej@gmail.com>
2021-05-21 19:02:12 +0200

    add navigation-bar
    add navigation-bar.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import LoginForm from './preact-components/login-form';
 import AlertBox from './preact-components/alert-box';
 import Card from './preact-components/card';
+import NavigationBar from './preact-components/navigation-bar';
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <LoginForm />
       <AlertBox />
       <Card />
---
+++ src/preact-components/navigation-bar.js
@@ -0,0 +1,11 @@
+export default () => {
+
+    return (
+        <ul className="navigation-bar">
+            <li><a href="/">Home</a></li>
+            <li><a href="/get-started">Get Started</a></li>
+            <li><a href="/donate">Donate</a></li>
+            <li className="special"><a href="/about-us">About us</a></li>
+        </ul>
+    )
+}

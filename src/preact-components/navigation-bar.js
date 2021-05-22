79098bd036e6e655f25e18e49be7578830bd3c3c
Sergej Sakac <sakacszergej@gmail.com>
2021-05-22 08:40:58 +0200

    added hamburger main-menu
    added hamburger main-menu for mobile phones.

--- src/preact-components/navigation-bar.js
+++ src/preact-components/navigation-bar.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 export default () => {
-
+    const [isOpen, setOpen] = useState(false)
+    const toggleOpen = () => {
+        setOpen(currentState => !currentState);
+    }
     return (
-        <ul className="navigation-bar">
-            <li><a href="/">Home</a></li>
-            <li><a href="/get-started">Get Started</a></li>
-            <li><a href="/donate">Donate</a></li>
-            <li className="special"><a href="/about-us">About us</a></li>
-        </ul>
+        <div>
+            <ul className="navigation-bar">
+                <li><a href="/">Home</a></li>
+                <li><a href="/get-started">Get Started</a></li>
+                <li><a href="/donate">Donate</a></li>
+                <li className="special"><a href="/about-us">About us</a></li>
+            </ul>
+            <div className="navigation-bar-mobile">
+                <div onClick={toggleOpen} className="navigation-toggle"></div>
+                {isOpen &&
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/get-started">Get Started</a></li>
+                        <li><a href="/donate">Donate</a></li>
+                        <li><a href="/about-us">About us</a></li>
+                        <li className="exit-button"><a onClick={toggleOpen}>X</a></li>
+                    </ul>
+                }
+            </div>
+        </div>
     )
 }

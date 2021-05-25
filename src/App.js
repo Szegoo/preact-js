7fc85cc127f6d86bcb433e1096f0e756e2c487d2
Sergej Sakac <sakacszergej@gmail.com>
2021-05-25 21:03:12 +0200

    added a hero image component
    added a hero image component. it has a nice parallel effect.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Card from './preact-components/card';
 import NavigationBar from './preact-components/navigation-bar';
 import Modal from './preact-components/modal';
 import Table from './preact-components/table';
+import HeroImage from './preact-components/heroImage';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <HeroImage />
       <LoginForm />
       <AlertBox />
       <Card />
---
+++ src/preact-components/heroImage.js
@@ -0,0 +1,21 @@
+export default () => {
+    return (
+        <div>
+            <h1 id="hero-text">Everything is possible.</h1>
+            <style>
+                {`
+                #hero-text {
+                    margin-top: 0;
+                    text-align: center;
+                    font-size: 2.5em;
+                    background: url("./images/hero-image.jpg");
+                    color: white;
+                    padding-top: 1.5em;
+                    padding-bottom: 2.5em;
+                    background-attachment: fixed;
+                }
+            `}
+            </style>
+        </div>
+    )
+}
